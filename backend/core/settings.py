0387ff6c57425c0dd1db2db661307a7023881f34
Abin-1703 <abinkuruvila123@gmail.com>
2026-03-05 19:30:53 +0530

    prepare backend deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR=Path(__file__).resolve().parent.parent
 SECRET_KEY="dev"
 DEBUG=False
-ALLOWED_HOSTS=[]
+ALLOWED_HOSTS=["*"]
 
 INSTALLED_APPS=[
 'django.contrib.admin',
@@ -24,12 +24,13 @@ MIDDLEWARE=[
 'corsheaders.middleware.CorsMiddleware',
 'django.middleware.common.CommonMiddleware',
 'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
 'django.contrib.sessions.middleware.SessionMiddleware',
 'django.middleware.common.CommonMiddleware',
 'django.middleware.csrf.CsrfViewMiddleware',
 'django.contrib.auth.middleware.AuthenticationMiddleware',
 'django.contrib.messages.middleware.MessageMiddleware',
-'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF='core.urls'
